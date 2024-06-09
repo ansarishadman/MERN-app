750b98ffdf69429a1ba74be1b6e971b4f86b62a3
Mohammad Ansari <mohammad.ansari@siemens-healthineers.com>
2024-06-09 17:25:33 +0530

    front end test setup added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,6 +42,24 @@ module.exports = {
         }
       },
       {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic'
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
